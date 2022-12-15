e111d734c91561aec54ff2657b0b3304396ab3af
mathisAlp <mathisalp.pro@gmail.com>
2022-12-15 14:33:56 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always O (success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
