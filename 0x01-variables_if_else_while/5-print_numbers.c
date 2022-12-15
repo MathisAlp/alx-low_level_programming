44092cea9c9db4e5f9130cd545626804e9b2aa4e
mathisAlp <mathisalp.pro@gmail.com>
2022-12-15 14:27:52 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always O (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
